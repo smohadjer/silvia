8fcbd196226cdda41244d0a8892ced4b4db16596
smohadjer <saeid@fastmail.fm>
2026-08-09 12:10:35 +0000

    Deploying to gh-pages from @ smohadjer/silvia@2578a8ded136c2f75040987e3c7959e61b7dbabf 🚀

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,28 +1,23 @@
-(function () {
-	'use strict';
-
-	//Helper classes to HTML for styling of nojs version
-	var html = document.querySelector('html');
-	html.classList.remove('no-js');
-	html.classList.add('js');
-
-	//taken from http://youmightnotneedjquery.com/
-	function ready(fn) {
-
-	  if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
-	    fn();
-	  } else {
-	    document.addEventListener('DOMContentLoaded', fn);
-	  }
-	}
-	ready(function () {
-
-	  console.log('DOM is ready!');
-	  var menu = document.querySelector('#menu');
-	  var wrapper = document.querySelector('#overall-wrapper');
-	  menu.addEventListener('click', function () {
-	    wrapper.classList.toggle('off-left');
-	  });
-	});
-
+(() => {
+  // app/resources/js/main.js
+  var html = document.querySelector("html");
+  html.classList.remove("no-js");
+  html.classList.add("js");
+  function ready(fn) {
+    "use strict";
+    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+      fn();
+    } else {
+      document.addEventListener("DOMContentLoaded", fn);
+    }
+  }
+  ready(function() {
+    "use strict";
+    console.log("DOM is ready!");
+    var menu = document.querySelector("#menu");
+    var wrapper = document.querySelector("#overall-wrapper");
+    menu.addEventListener("click", function() {
+      wrapper.classList.toggle("off-left");
+    });
+  });
 })();
