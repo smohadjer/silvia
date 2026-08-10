05e906d1a7e5d95fb720bfccc2df831a6a8af74b
Saeid Mohadjer <saeid@fastmail.fm>
2026-08-10 22:04:44 +0200

    small updates

--- app/resources/js/main.js
+++ app/resources/js/main.js
@@ -7,7 +7,7 @@ html.classList.add('js');
 function ready(fn) {
 	'use strict';
 
-	if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+	if (document.readyState !== 'loading') {
 		fn();
 	} else {
 		document.addEventListener('DOMContentLoaded', fn);
@@ -95,8 +95,6 @@ class Accordion {
 
 ready(function() {
 	'use strict';
-
-	console.log('DOM is ready!');
 
   var menu = document.querySelector('#menu');
   var wrapper = document.querySelector('#overall-wrapper');
