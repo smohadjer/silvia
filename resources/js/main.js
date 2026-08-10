1f770590bfdebcace91bb1b1222660a141f401e0
smohadjer <saeid@fastmail.fm>
2026-08-10 20:05:05 +0000

    Deploying to gh-pages from @ smohadjer/silvia@05e906d1a7e5d95fb720bfccc2df831a6a8af74b 🚀

--- resources/js/main.js
+++ resources/js/main.js
@@ -5,7 +5,7 @@
   html.classList.add("js");
   function ready(fn) {
     "use strict";
-    if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading") {
+    if (document.readyState !== "loading") {
       fn();
     } else {
       document.addEventListener("DOMContentLoaded", fn);
@@ -84,7 +84,6 @@
   };
   ready(function() {
     "use strict";
-    console.log("DOM is ready!");
     var menu = document.querySelector("#menu");
     var wrapper = document.querySelector("#overall-wrapper");
     menu.addEventListener("click", function() {
